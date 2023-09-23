2716123af46d52997c5b664547e0d7c174fc7649
Carlos Cerda <143562904+futuroinge@users.noreply.github.com>
2023-09-23 01:49:56 -0600

    Add files via upload

---
+++ EVA1_10_IDENTIFICADORES/src/main/java/com/mycompany/eva1_10_identificadores/EVA1_10_IDENTIFICADORES.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_10_identificadores;
+
+/**
+ *
+ * @author karlo
+ */
+public class EVA1_10_IDENTIFICADORES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       // int 1salario; no cumple con las reglas: inicia con número
+       //int salario&; no cumple con las reglas:el & no es un caracter alfanumérico
+       int mi_salario; 
+       int $salario;
+       int salario100;
+       //int_; no es valido
+       int $;
+       //int mi salario; no es valido, espacio en blanco
+       int salario;
+       int Salario;
+       int SALARIO;
+      //int salario; no se puede repetir identificadores
+      //int public; no podemos usar keywords (palabras reservada)
+    }
+}
